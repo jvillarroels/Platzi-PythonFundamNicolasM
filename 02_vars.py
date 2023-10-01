d15c702a3c9510aef6ea59404f9524d6e37bd6b5
Jose Villarroel <josedelvallevs@gmail.com>
2023-10-01 01:04:58 -0500

    Lección 5 Variables

---
+++ 02_vars.py
@@ -0,0 +1,18 @@
+# Lección 5 Variables
+print("Hola, vars")
+
+# Esto es una variable
+my_name = "Nicolas"
+print(my_name)
+
+# Esto es variable con un número
+my_age = 54
+print(my_age)
+
+my_name = "Santiago"
+print("aqui cambio ", my_name)
+
+# input
+
+my_name = input("Cuál es tu nombre?: ")
+print("Usando input: ", my_name)
