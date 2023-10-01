e505148cb027bdf2281637ea9e31705fee751057
Jose Villarroel <josedelvallevs@gmail.com>
2023-10-01 00:53:46 -0500

    Creando el main con instrucciones básicas

---
+++ main.py
@@ -0,0 +1,22 @@
+print("Hola, esto es Python")
+
+print("Hola, soy Jose y tengo 54 años")
+
+# Comentario entre lineas
+# Operaciones
+
+print(12 + 5)
+
+print(10 - 5)
+print(2 * 3)
+print(8 / 2)
+
+# Esto es un comentario
+
+"""
+varias lineas
+"""
+
+'''
+Otro de varias lineas
+'''
