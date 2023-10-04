8b188bfe1eb5dff749ea6c0e62dd5e8f025bbc7b
Jose Villarroel <josedelvallevs@gmail.com>
2023-10-04 00:24:52 -0500

    Clase 17 Operador lógico not

---
+++ 12_not.py
@@ -0,0 +1,20 @@
+print("")
+print("Programa 12_not.py")
+print("Clase 17 Operador lógico not")
+print("=====================")
+print("")
+
+print(not True)
+print(not False)
+
+# and
+print('NOT AND')
+print('not (True and True) => ', not (True and True))
+print('not (True and False) => ', not (True and False))
+print('not (False and True) => ', not (False and True))
+print('not (False and False) => ', not (False and False))
+
+stock = input('Ingrese el número de stock => ')
+stock = int(stock)
+
+print(not (stock >= 100 and stock <= 1000))
